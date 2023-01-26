c2f6549b4c8deade6453fb6e2293967ec98aee1e
Luis Angel Perez Melendez <angelmelen88@gmail.com>
2023-01-26 14:44:37 -0600

    prueba 2

---
+++ src/index.js
@@ -0,0 +1,31 @@
+console.log("si");
+
+const pais = document.querySelector('#pais');
+
+
+
+const estadosPais = [];
+
+    let estados = () => {
+
+            //crear opcion
+            const imagen = document.createElement('option');
+            
+            option.value="1"           
+            
+           
+
+            // Metemos todo dentro de opcion
+            const estados = document.createElement("option");
+            card.className = "md:flex bg-white rounded-lg p-6 hover:bg-gray-300";
+            card.append(imagen, priceAndTitle);
+
+            // Metemos todo dentro del contenedor principal
+            const contenedor = document.createElement("div");
+            contenedor.appendChild(card);
+
+            estadosPais.push(contenedor);
+        };
+        appNode.append(...todosLosItems)
+
+console.log('Happy hacking :)')
